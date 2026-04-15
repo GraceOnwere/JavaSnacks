d7bf54be56cd2c387616e7bc0b4c4978cba079d7
GraceOnwere <graceonwere.ng@gmail.com>
2026-04-15 17:04:20 +0100

    Added FireDrill Two files

---
+++ FireDrillTwo/TaskOne.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class TaskOne {
+public static void main(String[] args){
+
+    
+    
+
+    int score = 0;
+
+    int sum = 0;
+
+    Scanner inputCollector = new Scanner(System.in);
+
+    int number = inputCollector.nextInt();
+
+    
+    
+    /*System.out.println("Enter first number: ");
+
+    
+
+    System.out.println("Enter second number: ");
+
+    numberTwo = inputCollector.nextInt();
+
+    System.out.println("Enter third number: ");
+
+    numberThree = inputCollector.nextInt();
+
+    System.out.println("Enter fourth number: ");
+
+    numberFour = inputCollector.nextInt();
+
+    System.out.println("Enter fifth number: ");
+
+    numberFive = inputCollector.nextInt();
+*/
+
+    while(number >= 10;){
+   
+     System.out.println("Enter number: ");
+    
+    
+    //score++;
+    //sum +=number;    
+    //number++;
+    
+
+    System.out.println(number);
+    number++;
+    //System.out.println(score);
+    }
+
+}   
+}
