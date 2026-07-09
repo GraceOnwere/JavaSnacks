7cca2983615200adc90f0f66f2b7d918321f5c45
GraceOnwere <graceonwere.ng@gmail.com>
2026-07-09 22:26:17 +0100

    "Added bankAppTest unfinished version"

---
+++ IdeaProjects/javaOOP/test/bankApp/AccountTest.java
@@ -0,0 +1,116 @@
+package bankApp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AccountTest {
+
+    private Account graceAccount;
+    private final String pin = "1234";
+
+    @BeforeEach
+    public void setUp(){
+        graceAccount = new Account(pin);
+    }
+
+    @Test
+    public void balanceIsEmpty_balanceIsZeroTest() {
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(0, balance);
+    }
+
+    @Test
+    public void deposit200_balanceIs200Test(){
+
+        graceAccount.deposit(200);
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(200, balance);
+    }
+
+    @Test
+    public void depositMinus50_balanceIsZeroTest(){
+
+        graceAccount.deposit(-50);
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(0, balance);
+    }
+
+    @Test
+    public void deposit200and500_balanceIs700Test() {
+
+        graceAccount.deposit(200);
+        graceAccount.deposit(500);
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(700, balance);
+    }
+
+    @Test
+    public void withdraw200_throwsException_balanceIsZeroTest(){
+
+        assertThrows(IllegalArgumentException.class,()-> graceAccount.withdraw(200,pin));
+
+        assertEquals(0,graceAccount.getBalance(pin));
+    }
+
+    @Test
+    public void withdrawMinus200_throwsException_balanceIsZeroTest(){
+
+        assertThrows(IllegalArgumentException.class,()-> graceAccount.withdraw(-200,pin));
+
+        assertEquals(0,graceAccount.getBalance(pin));
+    }
+
+    @Test
+    public void deposit500_withdraw200WithCorrectPin_balanceIs300Test(){
+
+        graceAccount.deposit(500);
+        graceAccount.withdraw(200,pin);
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(300,balance);
+    }
+
+    @Test
+    public void deposit500_withdraw200WithIncorrectPin_throwsException_balanceIs500Test(){
+
+        graceAccount.deposit(500);
+
+        assertThrows(IllegalArgumentException.class,()-> graceAccount.withdraw(200,"1235"));
+
+        assertEquals(500,graceAccount.getBalance(pin));
+    }
+
+    @Test
+    public void deposit500_withdraw200and300Test(){
+
+        graceAccount.deposit(500);
+        graceAccount.withdraw(200,pin);
+        graceAccount.withdraw(300,pin);
+
+        int balance = graceAccount.getBalance(pin);
+
+        assertEquals(0,balance);
+    }
+
+    @Test
+    public void deposit500_withdraw1000_ThrowsExceptionTest(){
+
+        graceAccount.deposit(500);
+
+        assertThrows(IllegalArgumentException.class,()-> graceAccount.withdraw(1000, "1234"));
+    }
+
+
+}
---
+++ IdeaProjects/javaOOP/test/bankApp/BankTest.java
@@ -0,0 +1,17 @@
+//package bankApp;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class BankTest {
+//
+//    @Test
+//    public  void deposit500_balanceIs500(){
+//
+//        Bank zenithBank = new Bank("ZenithBank");
+//
+//        zenithBank.deposit(5000, 1);
+//
+//        zenithBank.getBalance(1,"1234");
+//    }
+//}
+//
